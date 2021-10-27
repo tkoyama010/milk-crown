05bef1c4f5fe07bddcc73efef2dad681730f6c52
Tetsuo Koyama <tkoyama010@gmail.com>
2021-10-27 15:21:34 +0900

    🏷️ Update to 3D mesh

--- demo_cahn-hilliard.py
+++ demo_cahn-hilliard.py
@@ -194,9 +194,9 @@ parameters["form_compiler"]["cpp_optimize"] = True
 # a pair of linear Lagrangian elements. ::
 
 # Create mesh and build function space
-mesh = UnitSquareMesh.create(96, 96, CellType.Type.quadrilateral)
+mesh = UnitSquareMesh.create(96, 96, 96, CellType.Type.quadrilateral)
 P1 = FiniteElement("Lagrange", mesh.ufl_cell(), 1)
-ME = FunctionSpace(mesh, P1*P1)
+ME = FunctionSpace(mesh, P1*P1*P1)
 
 # Trial and test functions of the space ``ME`` are now defined::
 
